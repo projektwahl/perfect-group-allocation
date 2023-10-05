1d1b94f3b81296b59effcee62ec483a530ae2538
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-10-05 21:31:59 +0200

    Use a nice solver library

---
+++ src/api.rs
@@ -0,0 +1,41 @@
+pub type TimeslotId = u8;
+
+pub struct Timeslot {
+    id: TimeslotId,
+}
+
+pub type WorkshopTopicId = u8;
+
+pub struct WorkshopTopic {
+    id: WorkshopTopicId,
+}
+
+pub type WorkshopId = u8;
+
+pub struct Workshop {
+    id: WorkshopId,
+    timeslot: TimeslotId,
+}
+
+pub type ParticipantId = u8;
+
+pub struct Participant {
+    id: ParticipantId,
+}
+
+pub type Rank = u8;
+
+pub struct Preference {
+    participant: ParticipantId,
+    topic: WorkshopTopicId,
+    rank: Rank,
+}
+
+pub type RoomID = u8;
+
+pub struct Room {
+    id: RoomID,
+}
+
+// manche workshops haben festgeschriebene räume (roomrequirements)
+// alle workshops präferenzen
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use good_lp::{
+    default_solver, solvers::ObjectiveDirection::Maximisation, variable, Expression, ModelWithSOS1,
+    ProblemVariables, Solution, SolverModel,
+};
+
+mod api;
+
 fn main() {
     println!("Hello, world!");
+
+    let mut variables = ProblemVariables::new();
+
+    let test = variables.add(variable().name("test").binary());
+    let vars = variables.add_vector(variable().name("awesome").binary(), 100);
+    let objective: Expression = vars.iter().sum();
+
+    println!("{}", variables.display(&objective));
+
+    let problem = variables.optimise(Maximisation, objective);
+
+    let solution = problem
+        .using(default_solver)
+        .with(test << 3)
+        .solve()
+        .unwrap();
+
+    println!("{}", solution.value(test));
 }
