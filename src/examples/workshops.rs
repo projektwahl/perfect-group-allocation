61de7950a0e653e77bdc68a65245e98be8757744
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-10-05 22:14:54 +0200

    ideas

--- src/examples/workshops.rs
+++ src/examples/workshops.rs
@@ -1,24 +1,36 @@
 // these will come from the database later
 // it needs to be ensured that the names are *globally* unique, so it makes sense to append an id there
-
 pub struct Requirement {
     pub identifier: String,
-}
-
-pub struct Room<'a> {
-    pub identifier: String,
-    pub requirements: Vec<&'a Requirement>,
 }
 
 pub struct Timeslot {
     pub identifier: String,
 }
 
+pub struct RoomSize(pub u8);
+
+pub struct Room<'a> {
+    pub identifier: String,
+    pub requirements: Vec<&'a Requirement>,
+    pub max_size: RoomSize,
+}
+
+/// Not every room may be available in every timeslot
+pub struct RoomInTimeSlot<'a> {
+    pub room: &'a Room<'a>,
+    pub timeslot: &'a Timeslot,
+}
+
+pub struct WorkshopTopicSize(pub u8);
+
 pub struct WorkshopTopic<'a> {
     pub identifier: String,
     pub requirements: Vec<&'a Requirement>,
+    pub max_size: WorkshopTopicSize,
 }
 
+// in theory if a person holds multiple workshops the system could decide which one should be held how many times. but we probably leave that problem for now.
 pub struct Workshop<'a> {
     pub topic: &'a WorkshopTopic<'a>,
     pub timeslot: &'a Timeslot,
@@ -28,7 +40,7 @@ pub struct Participant {
     pub identifier: String,
 }
 
-pub struct Rank(u8);
+pub struct Rank(pub u8);
 
 pub struct Preference<'a> {
     pub participant: &'a Participant,
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ use good_lp::{
     ProblemVariables, Solution, SolverModel,
 };
 
-use crate::examples::workshops::{Requirement, Room, Timeslot, Workshop, WorkshopTopic};
+use crate::examples::workshops::{
+    Participant, Preference, Rank, Requirement, Room, RoomInTimeSlot, RoomSize, Timeslot, Workshop,
+    WorkshopTopic, WorkshopTopicSize,
+};
 
 pub mod examples;
 
@@ -20,20 +23,6 @@ fn main() {
     };
     let requirements: Vec<&Requirement> = vec![&requirement_outside, &requirement_computer_pool];
 
-    let room_cpool = Room {
-        identifier: "C-Pool".to_string(),
-        requirements: vec![&requirement_computer_pool],
-    };
-    let room_bosch = Room {
-        identifier: "Bosch".to_string(),
-        requirements: vec![],
-    };
-    let room_draussen = Room {
-        identifier: "draussen".to_string(),
-        requirements: vec![&requirement_outside],
-    };
-    let rooms: Vec<&Room> = vec![&room_cpool, &room_bosch, &room_draussen];
-
     let timeslot_morgens = Timeslot {
         identifier: "morgens".to_string(),
     };
@@ -45,9 +34,35 @@ fn main() {
     };
     let timeslots: Vec<&Timeslot> = vec![&timeslot_morgens, &timeslot_mittags, &timeslot_abends];
 
+    let room_cpool = Room {
+        identifier: "C-Pool".to_string(),
+        requirements: vec![&requirement_computer_pool],
+        max_size: RoomSize(128),
+    };
+    let room_bosch = Room {
+        identifier: "Bosch".to_string(),
+        requirements: vec![],
+        max_size: RoomSize(75),
+    };
+    let room_draussen = Room {
+        identifier: "draussen".to_string(),
+        requirements: vec![&requirement_outside],
+        max_size: RoomSize(200),
+    };
+    let rooms: Vec<&Room> = vec![&room_cpool, &room_bosch, &room_draussen];
+    let rooms_in_timeslot: Vec<RoomInTimeSlot> = rooms
+        .iter()
+        .flat_map(|room| {
+            timeslots
+                .iter()
+                .map(|timeslot| RoomInTimeSlot { room, timeslot })
+        })
+        .collect();
+
     let workshop_topic_linux = WorkshopTopic {
         identifier: "linux-lernen".to_string(),
         requirements: vec![&requirement_computer_pool],
+        max_size: WorkshopTopicSize(50),
     };
     let workshop_topics: Vec<&WorkshopTopic> = vec![&workshop_topic_linux];
 
@@ -55,6 +70,19 @@ fn main() {
         topic: workshop_topics[0],
         timeslot: timeslots[0],
     }];
+
+    let participant_moritz = Participant {
+        identifier: "moritz".to_string(),
+    };
+    let participants: Vec<&Participant> = vec![&participant_moritz];
+
+    let preferences: Vec<&Preference> = vec![&Preference {
+        participant: &participant_moritz,
+        topic: &workshop_topic_linux,
+        rank: Rank(0),
+    }];
+
+    //
 
     let test = variables.add(variable().name("test").binary());
     let vars = variables.add_vector(variable().name("awesome").binary(), 100);
