b8cd966e8d5a2c7495774c6ade647ee7d0180834
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-10-11 00:41:10 +0200

    Jo

--- src/bin/server-warp.rs
+++ src/bin/server-warp.rs
@@ -1,6 +1,6 @@
 #![deny(warnings)]
 
-use warp::Filter;
+use warp::{filters::compression::brotli, Filter};
 
 #[tokio::main]
 async fn main() {
@@ -8,18 +8,20 @@ async fn main() {
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
         .with_span_events(tracing_subscriber::fmt::format::FmtSpan::FULL)
         .with_writer(std::io::stderr)
-        .with_max_level(tracing::Level::DEBUG)
+        .with_max_level(tracing::Level::INFO)
         .init();
 
     let html = include_str!("../../frontend/form.html");
 
-    let routes = warp::path::end()
-        .map(|| warp::reply::html(html.to_string()))
-        .or(warp::fs::dir("./frontend"));
+    let route1 = warp::path::end()
+        .and(warp::get())
+        .map(|| warp::reply::html(html.to_string()));
+
+    let route2 = warp::fs::dir("./frontend");
 
     println!("listening");
 
-    warp::serve(routes)
+    warp::serve(route1.or(route2).with(brotli()))
         .tls()
         .cert_path(".lego/certificates/h3.selfmade4u.de.crt")
         .key_path(".lego/certificates/h3.selfmade4u.de.key")
