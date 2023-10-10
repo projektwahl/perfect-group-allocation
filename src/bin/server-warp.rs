bcccbe3435f238801400546fbed6f9502d67a7ee
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-10-11 00:52:10 +0200

    Read form multipart

--- src/bin/server-warp.rs
+++ src/bin/server-warp.rs
@@ -1,6 +1,11 @@
 #![deny(warnings)]
 
-use warp::{filters::compression::brotli, Filter};
+use bytes::BufMut;
+use futures_util::TryStreamExt;
+use warp::{
+    filters::{compression::brotli, multipart::FormData},
+    Filter,
+};
 
 #[tokio::main]
 async fn main() {
@@ -17,11 +22,36 @@ async fn main() {
         .and(warp::get())
         .map(|| warp::reply::html(html.to_string()));
 
-    let route2 = warp::fs::dir("./frontend");
+    let route2 = warp::path::end()
+        .and(warp::post())
+        .and(warp::filters::multipart::form())
+        .and_then(|form: FormData| async move {
+            let field_names: Vec<_> = form
+                .and_then(|mut field| async move {
+                    let mut bytes: Vec<u8> = Vec::new();
+
+                    // field.data() only returns a piece of the content, you should call over it until it replies None
+                    while let Some(content) = field.data().await {
+                        let content = content.unwrap();
+                        bytes.put(content);
+                    }
+                    Ok((
+                        field.name().to_string(),
+                        String::from_utf8_lossy(&*bytes).to_string(),
+                    ))
+                })
+                .try_collect()
+                .await
+                .unwrap();
+
+            Ok::<_, warp::Rejection>(format!("{:?}", field_names))
+        });
+
+    let route3 = warp::fs::dir("./frontend");
 
     println!("listening");
 
-    warp::serve(route1.or(route2).with(brotli()))
+    warp::serve(route1.or(route2).or(route3).with(brotli()))
         .tls()
         .cert_path(".lego/certificates/h3.selfmade4u.de.crt")
         .key_path(".lego/certificates/h3.selfmade4u.de.key")
