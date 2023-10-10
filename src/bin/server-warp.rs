50dd50767b48bdd4156ae79e6ae096d8347fdfc3
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-10-11 00:31:32 +0200

    Make warp work

--- src/bin/server-warp.rs
+++ src/bin/server-warp.rs
@@ -1,23 +1,28 @@
 #![deny(warnings)]
 
-use http::Response;
+use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    use warp::Filter;
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .with_span_events(tracing_subscriber::fmt::format::FmtSpan::FULL)
+        .with_writer(std::io::stderr)
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
 
-    // Match any request and return hello world!
-    let routes = warp::any().map(|| {
-        Response::builder()
-            .header("alt-svc", r#"h3=":443"; ma=86400"#) // needs to be < 1024
-            .body("and a custom body")
-    });
+    let html = include_str!("../../frontend/form.html");
 
-    // warp doesn't support ec keys
+    let routes = warp::path::end()
+        .map(|| warp::reply::html(html.to_string()))
+        .or(warp::fs::dir("./frontend"));
+
+    println!("listening");
+
     warp::serve(routes)
         .tls()
         .cert_path(".lego/certificates/h3.selfmade4u.de.crt")
         .key_path(".lego/certificates/h3.selfmade4u.de.key")
-        .run(([127, 0, 0, 1], 443))
+        .run(([0, 0, 0, 0], 443))
         .await;
 }
