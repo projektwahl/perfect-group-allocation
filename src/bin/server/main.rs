7d9a7832882d45128e4269d6cf389147daa56d6a
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-10-12 18:47:51 +0200

    Clean up

--- src/bin/server/main.rs
+++ src/bin/server/main.rs
@@ -1,11 +1,10 @@
 mod entities;
 mod migrator;
-use entities::{prelude::*, *};
+use std::net::SocketAddr;
 
-use bytes::BufMut;
-use futures_util::StreamExt;
-use futures_util::TryFutureExt;
-use futures_util::TryStreamExt;
+use axum::routing::get;
+use axum::routing::post;
+use axum::Router;
 use sea_orm::ConnectionTrait;
 use sea_orm::Database;
 use sea_orm::DbBackend;
@@ -13,20 +12,11 @@ use sea_orm::DbErr;
 use sea_orm::Statement;
 use sea_orm_migration::MigratorTrait;
 use sea_orm_migration::SchemaManager;
-use warp::{
-    filters::{compression::brotli, multipart::FormData},
-    Filter,
-};
 
 use crate::migrator::Migrator;
 
 const DATABASE_URL: &str = "sqlite:./sqlite.db?mode=rwc";
 const DB_NAME: &str = "pga";
-
-#[derive(Debug)]
-struct InvalidParameter;
-
-impl warp::reject::Reject for InvalidParameter {}
 
 #[tokio::main]
 async fn main() -> Result<(), DbErr> {
@@ -75,61 +65,26 @@ async fn main() -> Result<(), DbErr> {
     Migrator::up(db, None).await?;
     assert!(schema_manager.has_table("project_history").await?);
 
-    // handlebars?
     let html = include_str!("../../../frontend/form.html");
 
-    let route1 = warp::path::end()
-        .and(warp::get())
-        .map(|| warp::reply::html(html.to_string()));
+    //  .cert_path(".lego/certificates/h3.selfmade4u.de.crt")
+    //  .key_path(".lego/certificates/h3.selfmade4u.de.key")
 
-    // we could rely on the order?
-    let route2 = warp::path::end()
-        .and(warp::post())
-        .and(warp::filters::multipart::form())
-        .then(|mut form: FormData| async move {
-            while let Some(item) = form.try_next().await.map_err(|err| )? {}
-            /*let field_names: Vec<_> = form
-                .and_then(|field| {
-                    let name = field.name().to_string();
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root))
+        // `POST /users` goes to `create_user`
+        .route("/users", post(create_user));
 
-                    match name.as_ref() {
-                        "title" => field.stream().try_fold(Vec::new(), |mut vec, data| {
-                            vec.put(data);
-                            async move { Ok(vec) }
-                        }),
-                        "description" => field.stream().try_fold(Vec::new(), |mut vec, data| {
-                            vec.put(data);
-                            async move { Ok(vec) }
-                        }),
-                        _ => {
-                            panic!();
-                        }
-                    }
-
-                    value.map_ok(move |vec| (name, vec))
-                })
-                .try_collect()
-                .await
-                .unwrap();
-
-            let project = project_history::ActiveModel {
-                ..Default::default()
-            };
-
-            Ok::<_, warp::Rejection>(format!("{:?}", field_names))*/
-            Ok::<_, warp::Error>("")
-        });
-
-    let route3 = warp::fs::dir("./frontend");
-
-    println!("listening");
-
-    warp::serve(route1.or(route2).or(route3).with(brotli()))
-        .tls()
-        .cert_path(".lego/certificates/h3.selfmade4u.de.crt")
-        .key_path(".lego/certificates/h3.selfmade4u.de.key")
-        .run(([0, 0, 0, 0], 8443)) // for http3 a port < 1024 is needed
-        .await;
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8443));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 
     Ok(())
 }
