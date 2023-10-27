6209c5d5a7a02f46b36f642993a220761c5bc429
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-10-28 01:23:15 +0200

    Jo

--- src/bin/server/routes/openid_redirect.rs
+++ src/bin/server/routes/openid_redirect.rs
@@ -23,6 +23,7 @@ pub struct OpenIdRedirectError {
 #[derive(Deserialize, Serialize)]
 pub struct OpenIdRedirectSuccess {
     state: String,
+    test: String,
 }
 
 #[derive(Deserialize)]
@@ -53,50 +54,52 @@ pub async fn openid_redirect(
             let result = handlebars
                 .render("openid_redirect", &err)
                 .unwrap_or_else(|e| e.to_string());
-            return Ok(Html(result).into_response());
+            Ok(Html(result).into_response())
         }
-        OpenIdRedirect::Success(_) => todo!(),
-    }
+        OpenIdRedirect::Success(_) => {
+            let session = session.lock().await;
+            let pkce_verifier = session.openid_pkce_verifier();
+            let nonce = session.openid_nonce();
+            drop(session);
 
-    let session = session.lock().await;
-    let pkce_verifier = session.openid_pkce_verifier();
-    let nonce = session.openid_nonce();
-    drop(session);
+            // Now you can exchange it for an access token and ID token.
+            let token_response = client
+                .exchange_code(AuthorizationCode::new(
+                    "some authorization code".to_string(),
+                ))
+                // Set the PKCE code verifier.
+                .set_pkce_verifier(pkce_verifier)
+                .request_async(async_http_client)
+                .await?;
 
-    // Now you can exchange it for an access token and ID token.
-    let token_response = client
-        .exchange_code(AuthorizationCode::new(
-            "some authorization code".to_string(),
-        ))
-        // Set the PKCE code verifier.
-        .set_pkce_verifier(pkce_verifier)
-        .request_async(async_http_client)
-        .await?;
+            // Extract the ID token claims after verifying its authenticity and nonce.
+            let id_token = token_response
+                .id_token()
+                .ok_or_else(|| anyhow!("Server did not return an ID token"))?;
+            let claims = id_token.claims(&client.id_token_verifier(), &nonce)?;
 
-    // Extract the ID token claims after verifying its authenticity and nonce.
-    let id_token = token_response
-        .id_token()
-        .ok_or_else(|| anyhow!("Server did not return an ID token"))?;
-    let claims = id_token.claims(&client.id_token_verifier(), &nonce)?;
+            // Verify the access token hash to ensure that the access token hasn't been substituted for
+            // another user's.
+            if let Some(expected_access_token_hash) = claims.access_token_hash() {
+                let actual_access_token_hash = AccessTokenHash::from_token(
+                    token_response.access_token(),
+                    &id_token.signing_alg()?,
+                )?;
+                if actual_access_token_hash != *expected_access_token_hash {
+                    Err(anyhow!("Invalid access token"))?;
+                }
+            }
 
-    // Verify the access token hash to ensure that the access token hasn't been substituted for
-    // another user's.
-    if let Some(expected_access_token_hash) = claims.access_token_hash() {
-        let actual_access_token_hash =
-            AccessTokenHash::from_token(token_response.access_token(), &id_token.signing_alg()?)?;
-        if actual_access_token_hash != *expected_access_token_hash {
-            Err(anyhow!("Invalid access token"))?;
+            // The authenticated user's identity is now available. See the IdTokenClaims struct for a
+            // complete listing of the available claims.
+            println!(
+                "User {} with e-mail address {} has authenticated successfully",
+                claims.subject().as_str(),
+                claims
+                    .email()
+                    .map_or("<not provided>", |email| email.as_str())
+            );
+            Ok(Redirect::to("/list").into_response())
         }
     }
-
-    // The authenticated user's identity is now available. See the IdTokenClaims struct for a
-    // complete listing of the available claims.
-    println!(
-        "User {} with e-mail address {} has authenticated successfully",
-        claims.subject().as_str(),
-        claims
-            .email()
-            .map_or("<not provided>", |email| email.as_str())
-    );
-    Ok(Redirect::to("/list").into_response())
 }
