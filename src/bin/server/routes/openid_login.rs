d5c265e832e38cbe9a6b6038d9c56d22b39fea28
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-10-28 22:13:45 +0200

    Interesting

--- src/bin/server/routes/openid_login.rs
+++ src/bin/server/routes/openid_login.rs
@@ -55,9 +55,7 @@ pub async fn openid_login(
             .url();
 
         let mut session = session.lock().await;
-        session.set_openid_pkce_verifier(&pkce_verifier);
-        session.set_openid_nonce(&nonce);
-        session.set_openid_csrf_token(&csrf_token);
+        session.set_openidconnect(&(&pkce_verifier, &nonce, &csrf_token));
         drop(session);
 
         Ok(Redirect::to(auth_url.as_str()).into_response())
