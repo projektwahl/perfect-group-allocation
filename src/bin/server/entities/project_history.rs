722f90226286e9afd3540a75e3f12f80f98383b2
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-11-20 00:50:44 +0100

    Fix autogen

--- src/bin/server/entities/project_history.rs
+++ src/bin/server/entities/project_history.rs
@@ -8,7 +8,7 @@ pub struct Model {
     #[sea_orm(primary_key)]
     pub row_id: i32,
     pub id: i32,
-    pub changed: String,
+    pub changed: DateTime,
     pub deleted: bool,
     pub author: i32,
     pub visibility: i32,
