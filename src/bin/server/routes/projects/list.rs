df88af3afbc2fa8b93329c563a22939f3d21ecfa
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-11-19 23:55:26 +0100

    wip

--- src/bin/server/routes/projects/list.rs
+++ src/bin/server/routes/projects/list.rs
@@ -1,3 +1,5 @@
+use alloc::borrow::Cow;
+
 use axum::body::StreamBody;
 use axum::extract::State;
 use axum::response::IntoResponse;
@@ -8,12 +10,32 @@ use html_escape::encode_safe;
 use hyper::header;
 use sea_orm::{DatabaseConnection, DbErr, EntityTrait};
 use serde_json::json;
+use zero_cost_templating::template_stream;
 
 use crate::entities::project_history;
 use crate::error::AppError;
 use crate::session::Session;
 use crate::templating::render;
 use crate::{TemplateProject, XRequestId};
+
+#[template_stream("templates/list.html.hbs")]
+pub async fn test2() {
+    let template = initial0!();
+    let template = template0!(template);
+    let page_title = "the>t&it<l\"e";
+    let template = page_title1!(template, page_title);
+    let csrf_token = "the>t&ok<e\"n";
+    let mut template = csrf_token2!(template, csrf_token);
+    let articles = get_articles_stream();
+    #[for_await]
+    for article in articles {
+        let inner_template = template3!(template);
+        let inner_template = title4!(inner_template, article);
+        template = text5!(inner_template, "twdhfewfe>et&ieft<l\"e");
+    }
+    let template = template6!(template);
+    copyright_year7!(template, "errhj>et&t<l\"e");
+}
 
 #[try_stream(ok = String, error = AppError)]
 async fn list_internal(db: DatabaseConnection, session: Session) {
