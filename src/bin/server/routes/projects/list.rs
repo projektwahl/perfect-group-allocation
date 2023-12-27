dab73b581468c9fa7bdc447be943f9afb66dc446
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-12-27 23:54:46 +0100

    revert

--- src/bin/server/routes/projects/list.rs
+++ src/bin/server/routes/projects/list.rs
@@ -1,15 +1,13 @@
-use alloc::borrow::Cow;
-
 use axum::body::StreamBody;
 use axum::extract::State;
 use axum::response::IntoResponse;
 use axum::TypedHeader;
+use futures_async_stream::try_stream;
 use futures_util::StreamExt;
 use html_escape::encode_safe;
 use hyper::header;
 use sea_orm::{DatabaseConnection, DbErr, EntityTrait};
 use serde_json::json;
-use zero_cost_templating::template_stream;
 
 use crate::entities::project_history;
 use crate::error::AppError;
@@ -17,12 +15,8 @@ use crate::session::Session;
 use crate::templating::render;
 use crate::{TemplateProject, XRequestId};
 
-#[template_stream("templates")]
-pub async fn test2(db: DatabaseConnection, session: Session) {
-    let template = list_initial0();
-}
-
-async gen fn list_internal(db: DatabaseConnection, session: Session) -> Result<String, AppError> {
+#[try_stream(ok = String, error = AppError)]
+async fn list_internal(db: DatabaseConnection, session: Session) {
     let stream = project_history::Entity::find().stream(&db).await?;
     yield render(&session, "main_pre", json!({"page_title": "Projects"})).await;
     #[for_await]
@@ -48,14 +42,14 @@ pub async fn list(
     TypedHeader(XRequestId(_request_id)): TypedHeader<XRequestId>,
     session: Session,
 ) -> (Session, impl IntoResponse) {
-    let stream = test2(db, session.clone()).map(|elem| Ok::<String, AppError>(elem.to_string())); /*.map(|elem| match elem {
-    Err(app_error) => Ok::<String, AppError>(format!(
-    // TODO FIXME use template here
-    "<h1>Error {}</h1>",
-    encode_safe(&app_error.to_string())
-    )),
-    Ok::<String, AppError>(ok) => Ok(ok),
-    });*/
+    let stream = list_internal(db, session.clone()).map(|elem| match elem {
+        Err(app_error) => Ok::<String, AppError>(format!(
+            // TODO FIXME use template here
+            "<h1>Error {}</h1>",
+            encode_safe(&app_error.to_string())
+        )),
+        Ok::<String, AppError>(ok) => Ok(ok),
+    });
     (
         session,
         (
