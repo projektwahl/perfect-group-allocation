e356bf3c4794bfe494d2728cbbe192f51dbe2aa4
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-12-28 20:26:18 +0100

    wip

--- src/bin/server/routes/favicon.rs
+++ src/bin/server/routes/favicon.rs
@@ -23,7 +23,7 @@ pub async fn favicon_ico(
 ) -> (Session, impl IntoResponse) {
     let etag_string = "\"xyzzy\"";
     let etag = etag_string.parse::<headers::ETag>().unwrap();
-    println!("{:?}", if_none_match);
+    println!("{if_none_match:?}");
 
     if if_none_match.precondition_passes(&etag) {
         (
--- src/bin/server/routes/indexcss.rs
+++ src/bin/server/routes/indexcss.rs
@@ -57,7 +57,7 @@ pub async fn indexcss(
 ) -> (Session, impl IntoResponse) {
     let etag_string = "\"xyzzy\"";
     let etag = etag_string.parse::<headers::ETag>().unwrap();
-    println!("{:?}", if_none_match);
+    println!("{if_none_match:?}");
     if if_none_match.precondition_passes(&etag) {
         (
             session,
