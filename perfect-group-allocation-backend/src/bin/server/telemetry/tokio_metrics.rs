68a8e6d62ddc48138f0f37380caa921b0a4401fd
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-01-01 09:47:33 +0100

    add metrics

--- perfect-group-allocation-backend/src/bin/server/telemetry/tokio_metrics.rs
+++ perfect-group-allocation-backend/src/bin/server/telemetry/tokio_metrics.rs
@@ -7,8 +7,10 @@ use std::pin::Pin;
 
 use axum::extract::{MatchedPath, Request};
 use http::Method;
+use opentelemetry::metrics::Unit;
+use opentelemetry::KeyValue;
 use pin_project::pin_project;
-use tokio_metrics::TaskMonitor;
+use tokio_metrics::{RuntimeMonitor, TaskMonitor};
 use tower::{Layer, Service};
 
 // TODO runtime metrics
@@ -90,4 +92,162 @@ where
         let this = self.project();
         this.response_future.poll(cx)
     }
+}
+
+pub fn tokio_runtime_metrics() {
+    let meter = opentelemetry::global::meter("perfect-group-allocation");
+    let handle = tokio::runtime::Handle::current();
+    let runtime_monitor = tokio_metrics::RuntimeMonitor::new(&handle);
+
+    let interval_root = std::sync::Mutex::new(runtime_monitor.intervals());
+
+    let workers_count = meter
+        .u64_observable_gauge("tokio.runtime_metrics.workers_count")
+        .init();
+
+    let total_park_count = meter
+        .u64_observable_gauge("tokio.runtime_metrics.total_park_count")
+        .init();
+    let max_park_count = meter
+        .u64_observable_gauge("tokio.runtime_metrics.max_park_count")
+        .init();
+    let min_park_count = meter
+        .u64_observable_gauge("tokio.runtime_metrics.min_park_count")
+        .init();
+    let mean_poll_duration = meter
+        .u64_observable_gauge("tokio.runtime_metrics.mean_poll_duration")
+        .with_unit(Unit::new("ns"))
+        .init();
+    let mean_poll_duration_worker_min = meter
+        .u64_observable_gauge("tokio.runtime_metrics.mean_poll_duration_worker_min")
+        .with_unit(Unit::new("ns"))
+        .init();
+    let mean_poll_duration_worker_max = meter
+        .u64_observable_gauge("tokio.runtime_metrics.mean_poll_duration_worker_max")
+        .with_unit(Unit::new("ns"))
+        .init();
+    //let poll_count_histogram = meter.u64_histogram("test").init();
+    let total_noop_count = meter
+        .u64_observable_gauge("tokio.runtime_metrics.total_noop_count")
+        .init();
+    let max_noop_count = meter
+        .u64_observable_gauge("tokio.runtime_metrics.max_noop_count")
+        .init();
+    let min_noop_count = meter
+        .u64_observable_gauge("tokio.runtime_metrics.min_noop_count")
+        .init();
+    let total_steal_count = meter
+        .u64_observable_gauge("tokio.runtime_metrics.total_steal_count")
+        .init();
+    let max_steal_count = meter
+        .u64_observable_gauge("tokio.runtime_metrics.max_steal_count")
+        .init();
+    let min_steal_count = meter
+        .u64_observable_gauge("tokio.runtime_metrics.min_steal_count")
+        .init();
+    let total_steal_operations = meter
+        .u64_observable_gauge("tokio.runtime_metrics.total_steal_operations")
+        .init();
+    let max_steal_operations = meter
+        .u64_observable_gauge("tokio.runtime_metrics.max_steal_operations")
+        .init();
+    let min_steal_operations = meter
+        .u64_observable_gauge("tokio.runtime_metrics.min_steal_operations")
+        .init();
+    let num_remote_schedules = meter
+        .u64_observable_gauge("tokio.runtime_metrics.num_remote_schedules")
+        .init();
+    let total_local_schedule_count = meter
+        .u64_observable_gauge("tokio.runtime_metrics.total_local_schedule_count")
+        .init();
+    let max_local_schedule_count = meter
+        .u64_observable_gauge("tokio.runtime_metrics.max_local_schedule_count")
+        .init();
+    let min_local_schedule_count = meter
+        .u64_observable_gauge("tokio.runtime_metrics.min_local_schedule_count")
+        .init();
+    let total_overflow_count = meter
+        .u64_observable_gauge("tokio.runtime_metrics.total_overflow_count")
+        .init();
+    let max_overflow_count = meter
+        .u64_observable_gauge("tokio.runtime_metrics.max_overflow_count")
+        .init();
+    let min_overflow_count = meter
+        .u64_observable_gauge("tokio.runtime_metrics.total_overflow_count")
+        .init();
+    let total_polls_count = meter
+        .u64_observable_gauge("tokio.runtime_metrics.total_polls_count")
+        .init();
+    let max_polls_count = meter
+        .u64_observable_gauge("tokio.runtime_metrics.max_polls_count")
+        .init();
+    let min_polls_count = meter
+        .u64_observable_gauge("tokio.runtime_metrics.min_polls_count")
+        .init();
+    let total_busy_duration = meter
+        .u64_observable_gauge("tokio.runtime_metrics.total_busy_duration")
+        .with_unit(Unit::new("ns"))
+        .init();
+    let max_busy_duration = meter
+        .u64_observable_gauge("tokio.runtime_metrics.max_busy_duration")
+        .with_unit(Unit::new("ns"))
+        .init();
+    let min_busy_duration = meter
+        .u64_observable_gauge("tokio.runtime_metrics.min_busy_duration")
+        .with_unit(Unit::new("ns"))
+        .init();
+    let injection_queue_depth = meter
+        .u64_observable_gauge("tokio.runtime_metrics.injection_queue_depth")
+        .init();
+    let total_local_queue_depth = meter
+        .u64_observable_gauge("tokio.runtime_metrics.total_local_queue_depth")
+        .init();
+    let max_local_queue_depth = meter
+        .u64_observable_gauge("tokio.runtime_metrics.max_local_queue_depth")
+        .init();
+    let min_local_queue_depth = meter
+        .u64_observable_gauge("tokio.runtime_metrics.min_local_queue_depth")
+        .init();
+    let elapsed = meter
+        .u64_observable_gauge("tokio.runtime_metrics.elapsed")
+        .with_unit(Unit::new("ns"))
+        .init();
+    let budget_forced_yield_count = meter
+        .u64_observable_gauge("tokio.runtime_metrics.budget_forced_yield_count")
+        .init();
+    let io_driver_ready_count = meter
+        .u64_observable_gauge("tokio.runtime_metrics.io_driver_ready_count")
+        .init();
+    meter
+        .register_callback(
+            &[
+                dropped_count.as_any(),
+                first_poll_count.as_any(),
+                instrumented_count.as_any(),
+                total_fast_poll_count.as_any(),
+                total_fast_poll_duration.as_any(),
+            ],
+            move |observer| {
+                let task_metrics = interval_root.lock().unwrap().next().unwrap();
+                let attrs = &[];
+                observer.observe_u64(&dropped_count, task_metrics.workers_count, attrs);
+                observer.observe_u64(&first_poll_count, task_metrics.first_poll_count, attrs);
+                observer.observe_u64(&instrumented_count, task_metrics.instrumented_count, attrs);
+                observer.observe_u64(
+                    &total_fast_poll_count,
+                    task_metrics.total_fast_poll_count,
+                    attrs,
+                );
+                observer.observe_u64(
+                    &total_fast_poll_duration,
+                    task_metrics
+                        .total_fast_poll_duration
+                        .as_nanos()
+                        .try_into()
+                        .unwrap(),
+                    attrs,
+                );
+            },
+        )
+        .unwrap();
 }
