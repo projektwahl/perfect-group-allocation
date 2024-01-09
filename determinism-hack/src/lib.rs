e868b1a1f5977c73e25de1c1562723f06e34689f
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-01-09 19:19:40 +0100

    wip

--- determinism-hack/src/lib.rs
+++ determinism-hack/src/lib.rs
@@ -1,17 +1,15 @@
-// ssize_t getrandom(void buf[.buflen], size_t buflen, unsigned int flags);
+use core::slice;
 
-use libc::{c_uint, c_void, memset, size_t, ssize_t};
+use libc::{c_uint, size_t, ssize_t};
+use rand::{RngCore, SeedableRng};
 
 /// # Safety
 /// Totally unsafe.
 #[allow(unsafe_code)]
 #[no_mangle]
 pub unsafe extern "C" fn getrandom(buf: *mut u8, buflen: size_t, _flags: c_uint) -> ssize_t {
-    #[allow(clippy::cast_possible_truncation)]
-    unsafe {
-        for i in 0..buflen {
-            *(buf.add(i)) = (i + buflen + 1337) as u8;
-        }
-    };
+    let slice = unsafe { slice::from_raw_parts_mut(buf, buflen) };
+    let mut rng = rand::rngs::StdRng::seed_from_u64(0);
+    rng.fill_bytes(slice);
     buflen.try_into().unwrap()
 }
