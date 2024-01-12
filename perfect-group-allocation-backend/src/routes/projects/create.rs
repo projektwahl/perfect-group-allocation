26680d2e3d0b40d286417c59c9fc2553bf1458c3
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-01-12 23:40:52 +0100

    wip

--- perfect-group-allocation-backend/src/routes/projects/create.rs
+++ perfect-group-allocation-backend/src/routes/projects/create.rs
@@ -6,11 +6,11 @@ use diesel_async::RunQueryDsl;
 use futures_util::StreamExt;
 use http::{header, Response, StatusCode};
 use http_body::{Body, Frame};
-use http_body_util::StreamBody;
+use http_body_util::{BodyExt, Empty, StreamBody};
 use perfect_group_allocation_css::index_css;
 use perfect_group_allocation_database::models::NewProject;
 use perfect_group_allocation_database::schema::project_history;
-use perfect_group_allocation_database::{DatabaseConnection, Pool};
+use perfect_group_allocation_database::{DatabaseConnection, DatabaseError, Pool};
 use tracing::error;
 use zero_cost_templating::async_iterator_extension::AsyncIteratorStream;
 use zero_cost_templating::{yieldoki, yieldokv, Unsafe};
@@ -18,7 +18,7 @@ use zero_cost_templating::{yieldoki, yieldokv, Unsafe};
 use crate::error::AppError;
 use crate::routes::create_project;
 use crate::session::Session;
-use crate::{yieldfi, yieldfv, CreateProjectPayload, CsrfSafeForm};
+use crate::{yieldfi, yieldfv, CreateProjectPayload, CsrfSafeForm, EitherBody};
 
 pub async fn create(
     request: hyper::Request<hyper::body::Incoming>,
@@ -29,6 +29,47 @@ pub async fn create(
     let form = CsrfSafeForm::<CreateProjectPayload>::from_request(request, session)
         .await
         .unwrap();
+
+    let empty_title = form.value.title.is_empty();
+    let empty_description = form.value.description.is_empty();
+    let mut global_error = None;
+
+    if !empty_title && !empty_description {
+        match pool.get().await {
+            Ok(mut connection) => {
+                if let Err(error) = diesel::insert_into(project_history::table)
+                    .values(NewProject {
+                        id: SystemTime::now()
+                            .duration_since(UNIX_EPOCH)
+                            .unwrap()
+                            .subsec_nanos()
+                            .try_into()
+                            .unwrap(),
+                        title: form.value.title.clone(),
+                        info: form.value.description.clone(),
+                    })
+                    .execute(&mut connection)
+                    .await
+                {
+                    global_error = Some(AppError::from(DatabaseError::from(error)).to_string());
+                };
+                return Ok(Response::builder()
+                    .status(if global_error.is_some() {
+                        StatusCode::INTERNAL_SERVER_ERROR
+                    } else {
+                        StatusCode::OK
+                    })
+                    .body(EitherBody::Left(
+                        Empty::new().map_err::<_, AppError>(|err| match err {}),
+                    ))
+                    .unwrap());
+            }
+            Err(error) => {
+                global_error = Some(AppError::from(error).to_string());
+            }
+        };
+    }
+
     let result = async gen move {
         let template = yieldfi!(create_project());
         let template = yieldfi!(template.next());
@@ -49,7 +90,6 @@ pub async fn create(
         let template = yieldfi!(template.next());
         let template = yieldfv!(template.title(form.value.title.clone()));
         let template = yieldfi!(template.next());
-        let empty_title = form.value.title.is_empty();
         let template = if empty_title {
             let template = yieldfi!(template.next_title_error_true());
             let template = yieldfv!(template.title_error("title must not be empty"));
@@ -59,7 +99,6 @@ pub async fn create(
         };
         let template = yieldfv!(template.description(form.value.description.clone()));
         let template = yieldfi!(template.next());
-        let empty_description = form.value.description.is_empty();
         let template = if empty_description {
             let template = yieldfi!(template.next_description_error_true());
             let template = yieldfv!(template.description_error("description must not be empty"));
@@ -68,42 +107,14 @@ pub async fn create(
             yieldfi!(template.next_description_error_false())
         };
         yieldfi!(template.next());
-
-        if empty_title || empty_description {
-            return;
-        }
-
-        let mut connection = match pool.get().await {
-            Ok(connection) => connection,
-            Err(error) => todo!(),
-        };
-
-        if let Err(error) = diesel::insert_into(project_history::table)
-            .values(NewProject {
-                id: SystemTime::now()
-                    .duration_since(UNIX_EPOCH)
-                    .unwrap()
-                    .subsec_nanos()
-                    .try_into()
-                    .unwrap(),
-                title: form.value.title.clone(),
-                info: form.value.description.clone(),
-            })
-            .execute(&mut connection)
-            .await
-        {
-            error!("{:?}", error);
-            yield Ok(Frame::data(Bytes::from_static(
-                b"TODO FIXME database error",
-            )));
-        };
-
-        // we can't stream the response and then redirect so probably add a button or so and use javascript? or maybe don't stream this page?
-        //Ok(Redirect::to("/list").into_response())
     };
     let stream = AsyncIteratorStream(result);
     Ok(Response::builder()
-        .status(StatusCode::OK)
-        .body(StreamBody::new(stream))
+        .status(if global_error.is_some() {
+            StatusCode::INTERNAL_SERVER_ERROR
+        } else {
+            StatusCode::OK
+        })
+        .body(EitherBody::Right(StreamBody::new(stream)))
         .unwrap())
 }
