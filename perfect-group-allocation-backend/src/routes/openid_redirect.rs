e37b2f0fc0fd4bfb152a138adedadf007e888ede
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-01-15 22:28:25 +0100

    wip

--- perfect-group-allocation-backend/src/routes/openid_redirect.rs
+++ perfect-group-allocation-backend/src/routes/openid_redirect.rs
@@ -31,86 +31,80 @@ pub struct OpenIdRedirectErrorTemplate {
 
 either_http_body!(either EitherBody 1 2);
 
-pub fn openid_redirect(
+pub async fn openid_redirect(
     request: hyper::Request<
         impl http_body::Body<Data = impl Buf + Send, Error = AppError> + Send + 'static,
     >,
     session: Session,
     config: Config,
-) -> impl Future<
-    Output = Result<
-        hyper::Response<impl Body<Data = Bytes, Error = Infallible> + Send + 'static>,
-        AppError,
-    >,
-> {
-    async move {
-        let body = request.uri().query().unwrap();
+) -> Result<hyper::Response<impl Body<Data = Bytes, Error = Infallible> + Send + 'static>, AppError>
+{
+    let body = request.uri().query().unwrap();
 
-        // TODO FIXME unwrap
-        let form: OpenIdRedirect<OpenIdRedirectInner> = serde_urlencoded::from_str(body).unwrap();
+    // TODO FIXME unwrap
+    let form: OpenIdRedirect<OpenIdRedirectInner> = serde_urlencoded::from_str(body).unwrap();
 
-        // what if privatecookiejar (and session?) would be non-owning (I don't want to clone them)
-        // TODO FIXME errors also need to return the session?
+    // what if privatecookiejar (and session?) would be non-owning (I don't want to clone them)
+    // TODO FIXME errors also need to return the session?
 
-        let expected_csrf_token = session.csrf_token();
+    let expected_csrf_token = session.csrf_token();
 
-        let (openid_session, session) = session.get_and_remove_temporary_openidconnect_state()?;
+    let (openid_session, session) = session.get_and_remove_temporary_openidconnect_state()?;
 
-        // Once the user has been redirected to the redirect URL, you'll have access to the
-        // authorization code. For security reasons, your code should verify that the `state`
-        // parameter returned by the server matches `csrf_state`.
+    // Once the user has been redirected to the redirect URL, you'll have access to the
+    // authorization code. For security reasons, your code should verify that the `state`
+    // parameter returned by the server matches `csrf_state`.
 
-        match form.inner {
-            OpenIdRedirectInner::Error(err) => {
-                let result = async gen move {
-                    let template = yieldfi!(crate::routes::openid_redirect());
-                    let template = yieldfi!(template.next());
-                    let template = yieldfi!(template.next());
-                    let template = yieldfv!(template.page_title("Create Project"));
-                    let template = yieldfi!(template.next());
-                    let template =
-                        yieldfv!(template.indexcss_version_unsafe(Unsafe::unsafe_input(
-                            index_css!().1.to_string()
-                        )));
-                    let template = yieldfi!(template.next());
-                    let template = yieldfi!(template.next());
-                    let template = yieldfi!(template.next_email_false());
-                    let template = yieldfv!(template.csrf_token(expected_csrf_token));
-                    let template = yieldfi!(template.next());
-                    let template = yieldfi!(template.next());
-                    let template = yieldfi!(template.next());
-                    let template = yieldfv!(template.error(err.error));
-                    let template = yieldfi!(template.next());
-                    let template = yieldfv!(template.error_description(err.error_description));
-                    let template = yieldfi!(template.next());
-                    yieldfi!(template.next());
-                };
-                let stream = AsyncIteratorStream(result);
-                Ok(Response::builder()
-                    .status(StatusCode::OK)
-                    .typed_header(ContentType::html())
-                    .body(EitherBody::Option1(StreamBody::new(stream)))
-                    .unwrap())
-            }
-            OpenIdRedirectInner::Success(ok) => {
-                let result = finish_authentication(
-                    config,
-                    serde_json::from_str(&openid_session).unwrap(),
-                    OpenIdRedirect {
-                        state: form.state,
-                        inner: ok,
-                    },
-                )
-                .await?;
+    match form.inner {
+        OpenIdRedirectInner::Error(err) => {
+            let result = async gen move {
+                let template = yieldfi!(crate::routes::openid_redirect());
+                let template = yieldfi!(template.next());
+                let template = yieldfi!(template.next());
+                let template = yieldfv!(template.page_title("Create Project"));
+                let template = yieldfi!(template.next());
+                let template = yieldfv!(
+                    template
+                        .indexcss_version_unsafe(Unsafe::unsafe_input(index_css!().1.to_string()))
+                );
+                let template = yieldfi!(template.next());
+                let template = yieldfi!(template.next());
+                let template = yieldfi!(template.next_email_false());
+                let template = yieldfv!(template.csrf_token(expected_csrf_token));
+                let template = yieldfi!(template.next());
+                let template = yieldfi!(template.next());
+                let template = yieldfi!(template.next());
+                let template = yieldfv!(template.error(err.error));
+                let template = yieldfi!(template.next());
+                let template = yieldfv!(template.error_description(err.error_description));
+                let template = yieldfi!(template.next());
+                yieldfi!(template.next());
+            };
+            let stream = AsyncIteratorStream(result);
+            Ok(Response::builder()
+                .status(StatusCode::OK)
+                .typed_header(ContentType::html())
+                .body(EitherBody::Option1(StreamBody::new(stream)))
+                .unwrap())
+        }
+        OpenIdRedirectInner::Success(ok) => {
+            let result = finish_authentication(
+                config,
+                serde_json::from_str(&openid_session).unwrap(),
+                OpenIdRedirect {
+                    state: form.state,
+                    inner: ok,
+                },
+            )
+            .await?;
 
-                let session = session.with_openidconnect_session(result);
+            let session = session.with_openidconnect_session(result);
 
-                Ok(Response::builder()
-                    .status(StatusCode::TEMPORARY_REDIRECT)
-                    .header(LOCATION, "/list")
-                    .body(EitherBody::Option2(Empty::new()))
-                    .unwrap())
-            }
+            Ok(Response::builder()
+                .status(StatusCode::TEMPORARY_REDIRECT)
+                .header(LOCATION, "/list")
+                .body(EitherBody::Option2(Empty::new()))
+                .unwrap())
         }
     }
 }
--- perfect-group-allocation-backend/src/routes/projects/create.rs
+++ perfect-group-allocation-backend/src/routes/projects/create.rs
@@ -26,119 +26,112 @@ either_http_body!(boxed EitherBody 1 2);
 
 // here we return a body that borrows the session. but the headers are already sent then to we have to implement the abstraction properly
 // maybe in this case it makes sense to clone for the body but still borrow for the head so you can set cookies before
-pub fn create<'a>(
+pub async fn create<'a>(
     request: hyper::Request<
         impl http_body::Body<Data = impl Buf + Send + 'static, Error = AppError> + Send + 'static,
     >,
     session: Session,
     pool: Pool,
-) -> impl Future<
-    Output = Result<
-        hyper::Response<impl Body<Data = Bytes, Error = Infallible> + Send + 'static>,
-        AppError,
-    >,
-> + 'a {
-    async move {
-        let form = CsrfSafeForm::<CreateProjectPayload>::from_request(request, &session)
-            .await
-            .unwrap();
+) -> Result<hyper::Response<impl Body<Data = Bytes, Error = Infallible> + Send + 'static>, AppError>
+{
+    let form = CsrfSafeForm::<CreateProjectPayload>::from_request(request, &session)
+        .await
+        .unwrap();
 
-        let empty_title = form.value.title.is_empty();
-        let empty_description = form.value.description.is_empty();
-        let mut global_error = None;
+    let empty_title = form.value.title.is_empty();
+    let empty_description = form.value.description.is_empty();
+    let mut global_error = None;
 
-        if !empty_title && !empty_description {
-            match pool.get().await {
-                Ok(mut connection) => {
-                    if let Err(error) = diesel::insert_into(project_history::table)
-                        .values(NewProject {
-                            id: SystemTime::now()
-                                .duration_since(UNIX_EPOCH)
-                                .unwrap()
-                                .subsec_nanos()
-                                .try_into()
-                                .unwrap(),
-                            title: form.value.title.clone(),
-                            info: form.value.description.clone(),
-                        })
-                        .execute(&mut connection)
-                        .await
-                    {
-                        global_error = Some(AppError::from(DatabaseError::from(error)).to_string());
-                    };
-                    return Ok(Response::builder()
-                        .status(if global_error.is_some() {
-                            StatusCode::INTERNAL_SERVER_ERROR
-                        } else {
-                            StatusCode::OK
-                        })
-                        .body(EitherBody::Option1(Empty::new()))
-                        .unwrap());
-                }
-                Err(error) => {
-                    global_error = Some(AppError::from(error).to_string());
-                }
-            };
-        }
-
-        let status_code = if global_error.is_some() {
-            StatusCode::INTERNAL_SERVER_ERROR
-        } else {
-            StatusCode::OK
+    if !empty_title && !empty_description {
+        match pool.get().await {
+            Ok(mut connection) => {
+                if let Err(error) = diesel::insert_into(project_history::table)
+                    .values(NewProject {
+                        id: SystemTime::now()
+                            .duration_since(UNIX_EPOCH)
+                            .unwrap()
+                            .subsec_nanos()
+                            .try_into()
+                            .unwrap(),
+                        title: form.value.title.clone(),
+                        info: form.value.description.clone(),
+                    })
+                    .execute(&mut connection)
+                    .await
+                {
+                    global_error = Some(AppError::from(DatabaseError::from(error)).to_string());
+                };
+                return Ok(Response::builder()
+                    .status(if global_error.is_some() {
+                        StatusCode::INTERNAL_SERVER_ERROR
+                    } else {
+                        StatusCode::OK
+                    })
+                    .body(EitherBody::Option1(Empty::new()))
+                    .unwrap());
+            }
+            Err(error) => {
+                global_error = Some(AppError::from(error).to_string());
+            }
         };
-
-        let result = async gen move {
-            let template = yieldfi!(create_project());
-            let template = yieldfi!(template.next());
-            let template = yieldfi!(template.next());
-            let template = yieldfv!(template.page_title("Create Project"));
-            let template = yieldfi!(template.next());
-            let template = yieldfv!(
-                template.indexcss_version_unsafe(Unsafe::unsafe_input(index_css!().1.to_string()))
-            );
-            let template = yieldfi!(template.next());
-            let template = yieldfi!(template.next());
-            let template = yieldfi!(template.next_email_false());
-            let template = yieldfv!(template.csrf_token(session.csrf_token()));
-            let template = yieldfi!(template.next());
-            let template = yieldfi!(template.next());
-            let template = yieldfi!(template.next());
-            let template = if let Some(global_error) = global_error {
-                let inner_template = yieldfi!(template.next_error_true());
-                let inner_template = yieldfv!(inner_template.error_message(global_error));
-                yieldfi!(inner_template.next())
-            } else {
-                yieldfi!(template.next_error_false())
-            };
-            let template = yieldfv!(template.csrf_token(session.csrf_token()));
-            let template = yieldfi!(template.next());
-            let template = if empty_title {
-                let template = yieldfi!(template.next_title_error_true());
-                let template = yieldfv!(template.title_error("title must not be empty"));
-                yieldfi!(template.next())
-            } else {
-                yieldfi!(template.next_title_error_false())
-            };
-            let template = yieldfv!(template.title(form.value.title.clone()));
-            let template = yieldfi!(template.next());
-            let template = if empty_description {
-                let template = yieldfi!(template.next_description_error_true());
-                let template =
-                    yieldfv!(template.description_error("description must not be empty"));
-                yieldfi!(template.next())
-            } else {
-                yieldfi!(template.next_description_error_false())
-            };
-            let template = yieldfv!(template.description(form.value.description.clone()));
-            let template = yieldfi!(template.next());
-
-            yieldfi!(template.next());
-        };
-        let stream = AsyncIteratorStream(result);
-        Ok(Response::builder()
-            .status(status_code)
-            .typed_header(ContentType::html())
-            .body(EitherBody::Option2(StreamBody::new(stream)))
-            .unwrap())
     }
+
+    let status_code = if global_error.is_some() {
+        StatusCode::INTERNAL_SERVER_ERROR
+    } else {
+        StatusCode::OK
+    };
+
+    let result = async gen move {
+        let template = yieldfi!(create_project());
+        let template = yieldfi!(template.next());
+        let template = yieldfi!(template.next());
+        let template = yieldfv!(template.page_title("Create Project"));
+        let template = yieldfi!(template.next());
+        let template = yieldfv!(
+            template.indexcss_version_unsafe(Unsafe::unsafe_input(index_css!().1.to_string()))
+        );
+        let template = yieldfi!(template.next());
+        let template = yieldfi!(template.next());
+        let template = yieldfi!(template.next_email_false());
+        let template = yieldfv!(template.csrf_token(session.csrf_token()));
+        let template = yieldfi!(template.next());
+        let template = yieldfi!(template.next());
+        let template = yieldfi!(template.next());
+        let template = if let Some(global_error) = global_error {
+            let inner_template = yieldfi!(template.next_error_true());
+            let inner_template = yieldfv!(inner_template.error_message(global_error));
+            yieldfi!(inner_template.next())
+        } else {
+            yieldfi!(template.next_error_false())
+        };
+        let template = yieldfv!(template.csrf_token(session.csrf_token()));
+        let template = yieldfi!(template.next());
+        let template = if empty_title {
+            let template = yieldfi!(template.next_title_error_true());
+            let template = yieldfv!(template.title_error("title must not be empty"));
+            yieldfi!(template.next())
+        } else {
+            yieldfi!(template.next_title_error_false())
+        };
+        let template = yieldfv!(template.title(form.value.title.clone()));
+        let template = yieldfi!(template.next());
+        let template = if empty_description {
+            let template = yieldfi!(template.next_description_error_true());
+            let template = yieldfv!(template.description_error("description must not be empty"));
+            yieldfi!(template.next())
+        } else {
+            yieldfi!(template.next_description_error_false())
+        };
+        let template = yieldfv!(template.description(form.value.description.clone()));
+        let template = yieldfi!(template.next());
+
+        yieldfi!(template.next());
+    };
+    let stream = AsyncIteratorStream(result);
+    Ok(Response::builder()
+        .status(status_code)
+        .typed_header(ContentType::html())
+        .body(EitherBody::Option2(StreamBody::new(stream)))
+        .unwrap())
 }
