8572a0119789abc1ca86a38da44c3b50f3c31988
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-01-18 23:51:03 +0100

    reduce dependencies

--- perfect-group-allocation-backend/src/bin/server/main.rs
+++ perfect-group-allocation-backend/src/bin/server/main.rs
@@ -1,7 +1,6 @@
 use perfect_group_allocation_backend::error::AppError;
 use perfect_group_allocation_backend::setup_http2_http3_server;
 use perfect_group_allocation_config::get_config;
-use perfect_group_allocation_telemetry::setup_telemetry;
 
 pub fn main() -> Result<(), AppError> {
     tokio::runtime::Builder::new_multi_thread()
@@ -9,9 +8,7 @@ pub fn main() -> Result<(), AppError> {
         .build()
         .unwrap()
         .block_on(async {
-            println!("main");
-
-            let _guard = setup_telemetry();
+            tracing_subscriber::fmt::init();
 
             setup_http2_http3_server(get_config()?).await?.await
         })
