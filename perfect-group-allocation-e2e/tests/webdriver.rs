8dd0cb34bf453513c3a0d488f3d53a50238bd5bc
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-02-02 15:34:31 +0100

    nice

--- perfect-group-allocation-e2e/tests/webdriver.rs
+++ perfect-group-allocation-e2e/tests/webdriver.rs
@@ -1,5 +1,11 @@
+use std::os::fd::{AsFd, AsRawFd};
+use std::process::{exit, Stdio};
+
 use perfect_group_allocation_backend::setup_http2_http3_server;
 use perfect_group_allocation_config::{Config, OpenIdConnectConfig};
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
+use tempfile::tempdir;
 use tracing::info;
 use webdriver_bidi::browsing_context::{self};
 use webdriver_bidi::input::perform_actions::{
@@ -19,10 +25,14 @@ use webdriver_bidi::{input, script, session, Browser, SendCommand, WebDriver};
 // so I think we should have n categories of setup (fully working, broken keycloak, crashed keycloak) that you can keep running between test setups? maybe some option to fully reset the database between runs (by restarting and deleting volume)?
 // for non-CI I think we should restart our server in between runs to get code updates
 
-// cargo run --bin webdriver
-#[tokio::main]
+#[tokio::test]
+pub async fn run_test() {
+    test().await.unwrap();
+}
+
+// cargo test --test 'webdriver' -- --nocapture
 #[allow(clippy::too_many_lines)]
-pub async fn main() -> Result<(), webdriver_bidi::Error> {
+pub async fn test() -> Result<(), webdriver_bidi::Error> {
     // https://docs.docker.com/compose/production/
 
     // https://www.redhat.com/sysadmin/quadlet-podman
@@ -36,12 +46,60 @@ pub async fn main() -> Result<(), webdriver_bidi::Error> {
 
     // I think we should not start it here like that but use podman to properly start it to minimize the differences. Some lower level testing may use this here?
 
-    let output = tokio::process::Command::new("ls")
-        .arg("-l")
-        .arg("-a")
+    let tmp_dir = tempdir().map_err(webdriver_bidi::ErrorInner::TmpDirCreate)?;
+
+    let rand_string: String = thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(10)
+        .map(char::from)
+        .collect();
+
+    let base_path = concat!(concat!(
+        "../..",
+        env!("CARGO_MANIFEST_DIR"),
+        "/../deployment/kustomize/base"
+    ));
+
+    let output = tokio::process::Command::new("/usr/bin/kustomize")
+        .arg("create")
+        .arg("--resources")
+        .arg(base_path)
+        .arg("--nameprefix")
+        .arg(rand_string)
+        .current_dir(tmp_dir.path())
         .output()
         .await
         .unwrap();
+
+    let kustomize_build = tokio::process::Command::new("/usr/bin/kustomize")
+        .arg("build")
+        .current_dir(tmp_dir.path())
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    let kustomize_build_stdout: Stdio = kustomize_build.stdout.unwrap().try_into().unwrap();
+
+    let podman_play = tokio::process::Command::new("podman")
+        .arg("kube")
+        .arg("play")
+        .arg("--replace")
+        .arg("--publish-all")
+        .arg("-")
+        .current_dir(tmp_dir.path())
+        .stdin(kustomize_build_stdout)
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    let output = podman_play.wait_with_output().await.unwrap();
+
+    println!("{}", std::str::from_utf8(&output.stdout).unwrap());
+    println!("{}", std::str::from_utf8(&output.stderr).unwrap());
+
+    println!("{:?}", tmp_dir);
+
+    exit(0);
 
     let driver = WebDriver::new(Browser::Firefox).await?;
     let _session = driver
