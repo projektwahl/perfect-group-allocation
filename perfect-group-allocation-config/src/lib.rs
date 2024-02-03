7243ea717727626cf459253436ede1a1892c0d4b
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-02-03 18:43:04 +0100

    nice errors

--- perfect-group-allocation-config/src/lib.rs
+++ perfect-group-allocation-config/src/lib.rs
@@ -1,5 +1,6 @@
 use core::fmt::{Debug, Display};
 use std::{
+    borrow::Cow,
     path::{Path, PathBuf},
     result,
     sync::Arc,
@@ -30,10 +31,10 @@ pub struct Config {
 
 #[derive(thiserror::Error)]
 pub enum ConfigError {
-    #[error("notify error {0}")]
-    Notify(#[from] notify::Error),
-    #[error("io error {0}")]
-    Io(#[from] std::io::Error),
+    #[error("notify error for path `{0}`: {1}")]
+    Notify(PathBuf, notify::Error),
+    #[error("io error for path `{0}`: {1}")]
+    Io(PathBuf, std::io::Error),
 }
 
 impl Debug for ConfigError {
@@ -42,17 +43,20 @@ impl Debug for ConfigError {
     }
 }
 
+async fn read_file(path: PathBuf) -> Result<String, ConfigError> {
+    tokio::fs::read_to_string(&path)
+        .await
+        .map_err(|e| ConfigError::Io(path, e))
+}
+
 pub async fn reread_config(config_directory: &Path) -> Result<Config, ConfigError> {
-    let url = tokio::fs::read_to_string(config_directory.join("url")).await?;
-    let database_url = tokio::fs::read_to_string(config_directory.join("database_url")).await?;
-    let issuer_url =
-        tokio::fs::read_to_string(config_directory.join("openidconnect/issuer_url")).await?;
-    let client_id =
-        tokio::fs::read_to_string(config_directory.join("openidconnect/client_id")).await?;
-    let client_secret =
-        tokio::fs::read_to_string(config_directory.join("openidconnect/client_secret")).await?;
-    let cert = tokio::fs::read_to_string(config_directory.join("tls/cert")).await?;
-    let key = tokio::fs::read_to_string(config_directory.join("tls/key")).await?;
+    let url = read_file(config_directory.join("url")).await?;
+    let database_url = read_file(config_directory.join("database_url")).await?;
+    let issuer_url = read_file(config_directory.join("openidconnect/issuer_url")).await?;
+    let client_id = read_file(config_directory.join("openidconnect/client_id")).await?;
+    let client_secret = read_file(config_directory.join("openidconnect/client_secret")).await?;
+    let cert = read_file(config_directory.join("tls/cert")).await?;
+    let key = read_file(config_directory.join("tls/key")).await?;
 
     Ok(Config {
         url,
@@ -72,7 +76,8 @@ pub async fn reread_config(config_directory: &Path) -> Result<Config, ConfigErro
 /// https://kubernetes.io/docs/tasks/inject-data-application/distribute-credentials-secure/#create-a-pod-that-has-access-to-the-secret-data-through-a-volume
 /// Secrets can be hot-reloaded so we can update configuration at runtime
 pub async fn get_config() -> Result<tokio::sync::watch::Receiver<Arc<Config>>, ConfigError> {
-    let config_directory = std::env::var_os("PGA_CONFIG_DIR").unwrap();
+    let config_directory =
+        std::env::var_os("PGA_CONFIG_DIR").expect("PGA_CONFIG_DIR env variable set");
     let config_directory = PathBuf::from(config_directory);
     let notify = Arc::new(tokio::sync::Notify::new());
     let notify2 = notify.clone();
@@ -83,9 +88,12 @@ pub async fn get_config() -> Result<tokio::sync::watch::Receiver<Arc<Config>>, C
             notify.notify_one();
         }
         Err(e) => println!("watch error: {:?}", e),
-    })?;
+    })
+    .map_err(|e| ConfigError::Notify(config_directory.clone(), e))?;
 
-    watcher.watch(&config_directory, RecursiveMode::Recursive)?;
+    watcher
+        .watch(&config_directory, RecursiveMode::Recursive)
+        .map_err(|e| ConfigError::Notify(config_directory.clone(), e))?;
 
     let (tx, rx) = tokio::sync::watch::channel(Arc::new(Config::default()));
     let config_directory2 = config_directory.clone();
