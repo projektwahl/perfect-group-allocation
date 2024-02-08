413dd90b404ccca8b075621d17ef97a5c5d47e7f
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-02-08 23:32:49 +0100

    wip

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -18,6 +18,7 @@ cp -r deployment/kustomize/* tmp/kustomize
 cd tmp/certs
 export CAROOT=$PWD
 CAROOT=$CAROOT mkcert -CAROOT
+CAROOT=$CAROOT mkcert -install # to allow local testing
 cd ../..
 
 cd tmp/kustomize
@@ -30,6 +31,8 @@ cargo build --test webdriver
 INTEGRATION_TEST_BINARY=$(cargo build --test webdriver --message-format json | jq --raw-output 'select(.reason == "compiler-artifact" and .target.name == "webdriver") | .executable')
 echo "Compiled integration test binary: $INTEGRATION_TEST_BINARY"
 
+# we need to use rootful podman to get routable ip addresses.
+
 # sudo podman inspect perfect-group-allocation-infra | jq ".[].NetworkSettings.Networks.[].IPAddress"
 # dig tmp-perfect-group-allocation @10.89.1.1
 # ping tmp-perfect-group-allocation
@@ -39,7 +42,7 @@ if [ "${1-}" == "keycloak" ]; then
     sudo podman build -t perfect-group-allocation --file base/perfect-group-allocation/Dockerfile .
     sudo podman build -t test --file base/test/Dockerfile .
 
-    (cd keycloak && CAROOT=$CAROOT mkcert keycloak)
+    (cd keycloak && CAROOT=$CAROOT mkcert tmp-keycloak)
     (cd keycloak && kustomize edit set nameprefix tmp-)
     (cd keycloak && kustomize edit add patch --patch '{"apiVersion": "v1","kind": "Pod","metadata":{"name":"keycloak"},"spec":{"volumes":[{"name":"root-ca","hostPath":{"path":"'"$CAROOT"'/rootCA.pem"}}]}}')
     (cd keycloak && kustomize build --output kubernetes.yaml)
@@ -50,14 +53,14 @@ if [ "${1-}" == "keycloak" ]; then
     echo keycloak started
     sudo podman exec tmp-keycloak-keycloak keytool -noprompt -import -file /run/rootCA.pem -alias rootCA -storepass password -keystore /tmp/.keycloak-truststore.jks
     sudo podman exec tmp-keycloak-keycloak /opt/keycloak/bin/kcadm.sh config truststore --trustpass password /tmp/.keycloak-truststore.jks
-    sudo podman exec tmp-keycloak-keycloak /opt/keycloak/bin/kcadm.sh config credentials --server https://keycloak --realm master --user admin --password admin
+    sudo podman exec tmp-keycloak-keycloak /opt/keycloak/bin/kcadm.sh config credentials --server https://tmp-keycloak --realm master --user admin --password admin
     sudo podman exec tmp-keycloak-keycloak /opt/keycloak/bin/kcadm.sh create realms -s realm=pga -s enabled=true
     sudo podman exec tmp-keycloak-keycloak /opt/keycloak/bin/kcadm.sh create users -r pga -s username=test -s email=test@example.com -s enabled=true
     sudo podman exec tmp-keycloak-keycloak /opt/keycloak/bin/kcadm.sh set-password -r pga --username test --new-password test
-    sudo podman exec tmp-keycloak-keycloak /opt/keycloak/bin/kcadm.sh create clients -r pga -s clientId=pga -s secret=$(cat base/client-secret) -s 'redirectUris=["https://perfect-group-allocation/openidconnect-redirect"]'
+    sudo podman exec tmp-keycloak-keycloak /opt/keycloak/bin/kcadm.sh create clients -r pga -s clientId=pga -s secret=$(cat base/client-secret) -s 'redirectUris=["https://tmp-perfect-group-allocation/openidconnect-redirect"]'
 fi
 
-(cd base && CAROOT=$CAROOT mkcert perfect-group-allocation)
+(cd base && CAROOT=$CAROOT mkcert tmp-perfect-group-allocation)
 (cd base && kustomize edit set nameprefix tmp-)
 (cd base && kustomize edit add patch --patch '{"apiVersion": "v1","kind": "Pod","metadata":{"name":"test"},"spec":{"volumes":[{"name":"test-binary","hostPath":{"path":"'"$INTEGRATION_TEST_BINARY"'"}}]}}')
 (cd base && kustomize edit add patch --patch '{"apiVersion": "v1","kind": "Pod","metadata":{"name":"perfect-group-allocation"},"spec":{"volumes":[{"name":"root-ca","hostPath":{"path":"'"$CAROOT"'/rootCA.pem"}}]}}')
