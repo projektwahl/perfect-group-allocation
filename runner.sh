6aade13bd14cbe99d391187affb75e6ee3cfe3dd
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-02-11 16:00:32 +0100

    closer

--- runner.sh
+++ runner.sh
@@ -2,6 +2,7 @@
 
 # FIXME path relative to cargo workspace root
 BINARY=$(realpath --relative-to=.. $1)
-IMAGE=$(podman build --quiet --build-arg BINARY=$BINARY --file ../deployment/kustomize/base/test/Dockerfile ..)
+echo $PWD
+IMAGE=$(podman build --quiet --build-arg CARGO_MANIFEST_DIR=$PWD --build-arg BINARY=$BINARY --file ../deployment/kustomize/base/test/Dockerfile ..)
 echo running $IMAGE
 podman run --device /dev/dri --userns=keep-id --user=$(id -u):$(id -g) -v /run/user/1000/wayland-0:/run/user/1000/wayland-0 $IMAGE
