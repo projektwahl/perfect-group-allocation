7507a502d06bab7aa9d31ff8555ad53bc1967312
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-02-17 15:50:03 +0100

    wip

--- runner.sh
+++ runner.sh
@@ -4,6 +4,11 @@ set -o nounset   # abort on unbound variable
 set -o pipefail  # don't hide errors within pipes
 set -x
 
-BINARY=$(realpath --relative-to=.. $1)
-IMAGE=$(podman build --build-arg CARGO_MANIFEST_DIR=$PWD --build-arg BINARY=$BINARY --file ../deployment/kustomize/base/test/Dockerfile ..)
-podman run --network podman-default-kube-network -v /run/user/1000/podman/podman.sock:/run/user/1000/podman/podman.sock --device /dev/dri -v /run/user/1000/wayland-0:/run/user/1000/wayland-0 --privileged --userns=keep-id --user=$(id -u):$(id -g) --rm $(echo "$IMAGE" | tail -n 1)
+BINARY=$(realpath --relative-to=. $1)
+
+CONTAINERIGNORE=$(mktemp)
+echo -e '*\n!'"$BINARY" > $CONTAINERIGNORE
+cat $CONTAINERIGNORE
+
+IMAGE=$(podman build --ignorefile $CONTAINERIGNORE --build-arg BINARY=$BINARY --file ./deployment/kustomize/base/test/Dockerfile .)
+podman run --network podman-default-kube-network --device /dev/dri -v /run/user/1000/wayland-0:/run/user/1000/wayland-0 --privileged --userns=keep-id --user=$(id -u):$(id -g) --rm $(echo "$IMAGE" | tail -n 1)
