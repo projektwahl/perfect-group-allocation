76e9f8b9ac064a7969c30a741d96412464fe1757
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-02-17 16:38:51 +0100

    wip

--- perfect-group-allocation-e2e/tests/integration.rs
+++ perfect-group-allocation-e2e/tests/integration.rs
@@ -1,12 +1,17 @@
+use std::process::ExitStatus;
+
 // cargo test -p perfect-group-allocation-e2e --test integration
 // https://doc.rust-lang.org/cargo/reference/environment-variables.html#environment-variables-cargo-sets-for-crates
 #[tokio::test]
 async fn it_adds_two() {
-    panic!("gonna run {}", env!("CARGO_BIN_EXE_webdriver"));
+    println!("gonna run {}", env!("CARGO_BIN_EXE_webdriver"));
 
-    Command::new("./runner.sh")
-        .args(env!("CARGO_BIN_EXE_webdriver"))
+    let status = tokio::process::Command::new("./runner.sh")
+        .current_dir("..")
+        .arg(env!("CARGO_BIN_EXE_webdriver"))
         .status()
         .await
         .unwrap();
+
+    assert!(status.success(), "process failed");
 }
