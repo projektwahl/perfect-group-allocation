e41259d032032f701b8c36b903934b78f6cce751
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-02-17 02:10:13 +0100

    wip

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -89,8 +89,8 @@ elif [ "${1-}" == "prepare" ]; then
 
     # git describe --always --long --dirty 
     # TODO URGENT FIXME: Reduce build context size
-    echo -e '*\n!server' > $SERVER_BINARY_DIR/.containerignore
-    SERVER_IMAGE=$(podman --remote build --build-arg BINARY=./server --file $PWD/deployment/kustomize/base/perfect-group-allocation/Dockerfile $SERVER_BINARY_DIR)
+    echo -e '*\n!target/debug/server\n!deployment/kustomize/base/perfect-group-allocation/Dockerfile' > $PWD/.containerignore
+    SERVER_IMAGE=$(podman --log-level=debug --remote build --build-arg BINARY=./target/debug/server --file ./deployment/kustomize/base/perfect-group-allocation/Dockerfile $PWD)
     kustomize edit set image perfect-group-allocation=sha256:$(echo "$SERVER_IMAGE" | tail -n 1)
     #TEST_IMAGE=$(podman build --quiet --build-arg BINARY=$INTEGRATION_TEST_BINARY --file ./deployment/kustomize/base/test/Dockerfile ..)
     #kustomize edit set image test=sha256:$TEST_IMAGE
