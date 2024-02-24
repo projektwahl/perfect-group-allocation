8d5bb7d3ed33fa7c0103b8b32fe3baff05f35951
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-02-24 21:55:29 +0000

    Jo

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -15,10 +15,6 @@ mkdir -p rootca
 export CAROOT=$PWD/rootca
 GARBAGE=$(mktemp -d)
 
-cd $GARBAGE
-mkcert force-root-ca-gen && rm ./force-root-ca-gen.pem ./force-root-ca-gen-key.pem
-cp $CAROOT/rootCA.pem .
-
 # we need to use rootful podman to get routable ip addresses.
 
 # https://kubernetes.io/docs/tasks/run-application/run-single-instance-stateful-application/
@@ -31,8 +27,9 @@ cp $CAROOT/rootCA.pem .
 echo -n myawesomeclientsecret > client-secret
 
 if [ "${1-}" == "keycloak" ]; then
+    cd $GARBAGE
+
     kustomize create
-    kustomize edit add configmap root-ca --from-file=./rootCA.pem
 
     KEYCLOAK_CONTAINERIGNORE=$(mktemp)
     echo -e '*' > $KEYCLOAK_CONTAINERIGNORE
@@ -47,6 +44,9 @@ if [ "${1-}" == "keycloak" ]; then
     kustomize edit add resource ../../$PROJECT/deployment/kustomize/keycloak
 
     mkcert "${KEYCLOAK_PREFIX}keycloak"
+    cp $CAROOT/rootCA.pem .
+    kustomize edit add configmap root-ca --from-file=./rootCA.pem
+
     kustomize edit add secret keycloak-tls-cert \
         --type=kubernetes.io/tls \
         --from-file=tls.crt=./${KEYCLOAK_PREFIX}keycloak.pem \
@@ -75,9 +75,11 @@ if [ "${1-}" == "keycloak" ]; then
     # https://github.com/keycloak/keycloak/discussions/9278
     echo DO NOT RUN THIS IN PRODUCTION!!!
     podman exec ${KEYCLOAK_PREFIX}keycloak-keycloak /opt/keycloak/bin/kcadm.sh create clients -r pga -s clientId=pga -s secret=$(cat client-secret) -s 'redirectUris=["*"]'
-else
+else if [ "${1-}" == "backend-db-and-test" ]; then
+    cargo build --bin server
+    cd $GARBAGE
+
     kustomize create
-    kustomize edit add configmap root-ca --from-file=./rootCA.pem
 
     SERVER_CONTAINERIGNORE=$(mktemp)
     echo -e '*\n!target/debug/server\n!deployment/kustomize/base/perfect-group-allocation/Dockerfile' > $SERVER_CONTAINERIGNORE
@@ -89,6 +91,9 @@ else
     kustomize edit set nameprefix $PREFIX
 
     mkcert "${PREFIX}perfect-group-allocation" # maybe use a wildcard certificate instead? to speed this up
+    cp $CAROOT/rootCA.pem .
+    kustomize edit add configmap root-ca --from-file=./rootCA.pem
+
     kustomize edit add secret application-config \
         --from-file=tls.crt=./${PREFIX}perfect-group-allocation.pem \
         --from-file=tls.key=./${PREFIX}perfect-group-allocation-key.pem \
@@ -112,4 +117,48 @@ else
     podman logs --color --names --follow ${PREFIX}test-test ${PREFIX}perfect-group-allocation-perfect-group-allocation & #${KEYCLOAK_PREFIX}keycloak-keycloak & # ${PREFIX}postgres-postgres
     (exit $(podman wait ${PREFIX}test-test))
     podman kube down --force kubernetes.yaml || true # WARNING: this also removes volumes
+else if [ "${1-}" == "backend" ]; then
+    cargo build --bin server
+    cd $GARBAGE
+
+    kustomize create
+
+    SERVER_CONTAINERIGNORE=$(mktemp)
+    echo -e '*\n!target/debug/server\n!deployment/kustomize/base/perfect-group-allocation/Dockerfile' > $SERVER_CONTAINERIGNORE
+    # tag with: git describe --always --long --dirty 
+    SERVER_IMAGE=$(podman build --ignorefile $SERVER_CONTAINERIGNORE --build-arg BINARY=./target/debug/server --file ./deployment/kustomize/base/perfect-group-allocation/Dockerfile $PROJECT)
+    kustomize edit set image perfect-group-allocation=sha256:$(echo "$SERVER_IMAGE" | tail -n 1)
+
+    kustomize edit add resource ../../$PROJECT/deployment/kustomize/base/
+    kustomize edit set nameprefix $PREFIX
+
+    mkcert "${PREFIX}perfect-group-allocation" # maybe use a wildcard certificate instead? to speed this up
+    cp $CAROOT/rootCA.pem .
+    kustomize edit add configmap root-ca --from-file=./rootCA.pem
+
+    kustomize edit add secret application-config \
+        --from-file=tls.crt=./${PREFIX}perfect-group-allocation.pem \
+        --from-file=tls.key=./${PREFIX}perfect-group-allocation-key.pem \
+        --from-file=openidconnect.client_secret=./client-secret \
+        --from-literal=openidconnect.client_id=pga \
+        --from-literal=openidconnect.issuer_url=https://${KEYCLOAK_PREFIX}keycloak/realms/pga \
+        --from-literal="database_url=postgres://postgres:bestpassword@postgres/pga" \
+        --from-literal=url=https://${PREFIX}perfect-group-allocation.dns.podman
+
+    INTEGRATION_TEST_BINARY=$(realpath --relative-to=$PROJECT $1)
+    INTEGRATION_TEST_CONTAINERIGNORE=$(mktemp)
+    echo -e '*\n!'"$INTEGRATION_TEST_BINARY" > $INTEGRATION_TEST_CONTAINERIGNORE
+    INTEGRATION_TEST_IMAGE=$(podman build --ignorefile $INTEGRATION_TEST_CONTAINERIGNORE --build-arg BINARY=$INTEGRATION_TEST_BINARY --build-arg EXECUTABLE=$2 --file ./deployment/kustomize/base/test/Dockerfile $PROJECT)
+    INTEGRATION_TEST_IMAGE=$(echo "$INTEGRATION_TEST_IMAGE" | tail -n 1)
+    kustomize edit set image test=sha256:$INTEGRATION_TEST_IMAGE
+
+    kustomize build --output kubernetes.yaml
+    podman kube down --force kubernetes.yaml || true # WARNING: this also removes volumes
+    podman kube play kubernetes.yaml # ahh kube uses another network
+    #echo https://${PREFIX}perfect-group-allocation.dns.podman
+    podman logs --color --names --follow ${PREFIX}test-test ${PREFIX}perfect-group-allocation-perfect-group-allocation & #${KEYCLOAK_PREFIX}keycloak-keycloak & # ${PREFIX}postgres-postgres
+    (exit $(podman wait ${PREFIX}test-test))
+    podman kube down --force kubernetes.yaml || true # WARNING: this also removes volumes
+else
+    echo "unknown command"
 fi
