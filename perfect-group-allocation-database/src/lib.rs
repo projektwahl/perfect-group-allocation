ff721cd215bf140c2d375ca0beb85a8042d9412f
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-02-24 14:50:23 +0000

    wip

--- perfect-group-allocation-database/src/lib.rs
+++ perfect-group-allocation-database/src/lib.rs
@@ -3,9 +3,10 @@ pub mod models;
 pub mod schema;
 
 use diesel::prelude::*;
+use diesel_async::async_connection_wrapper::AsyncConnectionWrapper;
 use diesel_async::pooled_connection::deadpool::{Object, Pool as DeadPool};
 use diesel_async::pooled_connection::AsyncDieselConnectionManager;
-use diesel_async::{AsyncPgConnection, RunQueryDsl};
+use diesel_async::{AsyncConnection, AsyncPgConnection, RunQueryDsl};
 pub use error::DatabaseError;
 use schema::project_history;
 
@@ -13,16 +14,32 @@ use crate::models::ProjectHistoryEntry;
 
 pub type Pool = DeadPool<AsyncPgConnection>;
 
-pub fn get_database_connection(database_url: &str) -> Result<Pool, DatabaseError> {
-    let config = AsyncDieselConnectionManager::<diesel_async::AsyncPgConnection>::new(database_url);
-    Ok(DeadPool::builder(config).build()?)
+use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
+
+pub async fn get_database_connection(database_url: String) -> Result<Pool, DatabaseError> {
+    let config =
+        AsyncDieselConnectionManager::<diesel_async::AsyncPgConnection>::new(&database_url);
+
+    let pool = DeadPool::builder(config).build()?;
+
+    // TODO do this in a once cell in case the database is not available on startup
+    tokio::task::spawn_blocking(move || {
+        let mut connection =
+            AsyncConnectionWrapper::<AsyncPgConnection>::establish(&database_url).unwrap();
+        connection.run_pending_migrations(MIGRATIONS).unwrap();
+    })
+    .await
+    .unwrap();
+
+    Ok(pool)
 }
 
 pub struct DatabaseConnection(pub Object<AsyncPgConnection>);
 
 pub async fn example() -> Result<(), DatabaseError> {
     let database_url = std::env::var("DATABASE_URL")?;
-    let pool = get_database_connection(&database_url)?;
+    let pool = get_database_connection(database_url).await?;
 
     // checkout a connection from the pool
     let mut connection = pool.get().await?;
